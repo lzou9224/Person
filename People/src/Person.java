e30aff2527d60d17cd9a1001916dcf79c3fe16a3
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-11-21 09:01:50 -0500

    :)

---
+++ People/src/Person.java
@@ -0,0 +1,73 @@
+
+public abstract class Person
+{
+	private int height; // inches
+	private String firstName, familyName; 
+	private double weight; //pounds
+	
+	public Person (int height, String firstName, String familyName, double weight)
+	{
+		this.height=height;
+		this.firstName=firstName;
+		this.familyName=familyName;
+		this.weight=weight;
+	}
+	
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the familyName
+	 */
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	/**
+	 * @param familyName the familyName to set
+	 */
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public abstract String toString();
+}
