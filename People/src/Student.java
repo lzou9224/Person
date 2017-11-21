89dfa234ade6e9895e4fd3afa15db0b945db6001
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-11-21 09:25:48 -0500

    sdff

--- People/src/Student.java
+++ People/src/Student.java
@@ -1,4 +1,73 @@
 
-public class Student {
+public class Student Extends Person
+{
+	private double GPA;
+	private String Major;
+	private int OSIS;
+	
+	public Student(double GPA, String Major, int OSIS)
+	{
+		super(GPA, Major, OSIS); //super constructor
+		this.GPA=GPA;
+		this.Major=Major;
+		this.OSIS=OSIS;
+		
+	}
 
+	/**
+	 * @return the person
+	 */
+	public Extends getPerson() {
+		return Person;
+	}
+
+	/**
+	 * @param person the person to set
+	 */
+	public void setPerson(Extends person) {
+		Person = person;
+	}   
+
+	/**
+	 * @return the gPA
+	 */
+	public double getGPA() {
+		return GPA;
+	}
+
+	/**
+	 * @param gPA the gPA to set
+	 */
+	public void setGPA(double gPA) {
+		GPA = gPA;
+	}
+
+	/**
+	 * @return the major
+	 */
+	public String getMajor() {
+		return Major;
+	}
+
+	/**
+	 * @param major the major to set
+	 */
+	public void setMajor(String major) {
+		Major = major;
+	}
+
+	/**
+	 * @return the oSIS
+	 */
+	public int getOSIS() {
+		return OSIS;
+	}
+
+	/**
+	 * @param oSIS the oSIS to set
+	 */
+	public void setOSIS(int oSIS) {
+		OSIS = oSIS;
+	}
 }
+
--- People/src/Teacher.java
+++ People/src/Teacher.java
@@ -1,4 +1,50 @@
 
-public class Teacher {
+public class Teacher Extends Person 
+{
+	private String Subject;
+	private String Title; //Mr, Ms, Mx, Mrs
+	
+	public Teacher(String Subject, String Title)
+	{
+		super(Subject, Title); //super constructor
+		this.Subject=Subject;
+		this.Title=Title;
+	}
+	public abstract String toString();
 
+
+	/**
+	 * @param person the person to set
+	 */
+	public void setPerson(Extends person) {
+		Person = person;
+	}
+
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return Subject;
+	}
+
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		Subject = subject;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return Title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		Title = title;
+	}
 }
