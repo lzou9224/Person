98eb6a268d63e5bc4ee5709b8d079a1fc44a2427
Nasrin Khan <Nasrin Khan@192.168.2.7>
2017-11-30 05:39:26 -0500

    ahsj

--- People/src/Runner.java
+++ People/src/Runner.java
@@ -8,10 +8,40 @@ public class Runner {
 	public static void main(String[]args)
 	{
 		private static String[] firstNames();
-		private static String[] famileName();
+		private static String[] familyName();
 		private static String[] subject();
+		private static Teacher teacher;
+		private static Student[] students;
 		
+		Teacher teacher1 = new Teacher (10, "Tom", "Moore", 91.3, "science", "Mr.");
 		
+	    Student student1 = new Student (3, "Lisa", "Smith", 97.2, 208644062, 9);
+	    
+	    Classroom classroom1 = new Classroom (students, teacher, subject );
+		
+	    public static void main (String [] args) 
+	    {
+			 firstNames=new String [4];
+			 
+			 familyNames=new String [4];
+			 
+			 firstNames [0] ="Eliza";
+			 
+			 familyNames [0]="Marie";
+			 
+			 firstNames [1]="Annie";
+			 
+			 familyNames [1]="Mac";
+			 
+			 firstNames [2]="Loura";
+			 
+			 familyNames [2]="Johnson";
+			 
+			 firstNames [3]="Brady";
+			 
+			 familyNames [3]="Williams";
+	    }
 
+	}
 	
 }
