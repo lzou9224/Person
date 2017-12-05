34db0461ab1508b13291e42f9c986e087002d808
Nasrin Khan <Nasrin Khan@192.168.2.6>
2017-12-05 06:31:38 -0500

    sd

--- People/src/Runner.java
+++ People/src/Runner.java
@@ -5,63 +5,70 @@
  */
 
 public class Runner {
-	public static void main(String[]args)
-	{
-		private static String[] firstNames() ={"Sara", "Lingli", "Tabassum", "Sam", "Alex", "John",
+	{  
+		public static void main(String[] args)
+		{
+			   Classroom NewClass = new Classroom(student, teacher);
+			   NewClass.printClass();
+			   System.out.println(NewClass.classAverage());
+		}
+		public static Student randomStudent()	
+		{
+			Random Student = new Random(); 
+			String firstName = firstNames[Student.nextInt(firstNames.length)];
+			String familyName = familyNames[Student.nextInt(familyNames.length)];
+			double GPA = Student.nextDouble()*4.0;
+			String Major = Majors[Student.nextInt(Majors.length)];
+			String Guidance = Guidance[Student. nextInt(Guidance.length)];
+			
+			return new Student(firstName, familyName, major, guidance, GPA);
+			
+			}
+		 //List of variables of a student
+		 String lastName[]= {"Sara", "Lingli", "Tabassum", "Sam", "Alex", "John",
 				"Nicole", "Vivian", "Ming", "Kelly", "Jenny", "Daniel", "Tanvir", "Amy", "David",
 				"Dustin", "Sam", "Mimi", "Fanny", "Bob", "Sherry", "Michelle", "Toby", "Felix", 
 				"Tammy", "William", "Thomas", "Allen", "Kenny", "Kevin"};
 		
-		private static String[] familyNames()={"Zou", "Taylor", "Brooks", "Williams", "Smith", "Johnson", "Jones",
+		 String firstName[]= {"Zou", "Taylor", "Brooks", "Williams", "Smith", "Johnson", "Jones",
 				"Lin", "Thompson", "Brown", "Davis", "Lee", "Hill", "Scott", "Lopez", "Mitchell", "Perez",
 				"Roberts", "Turner", "Edwards", "Morris", "Rogers", "Reed", "Cooper", "Cox", "Torres", "James",
 				"Bhuiyan", "Wood", "Chen"};
 		
-		private static String[] Subject()={"Math", "Science", "Technology", "Computer Sciences", "Calculus", "Algebra",
+		 String Subject[] = {"Math", "Science", "Technology", "Computer Sciences", "Calculus", "Algebra",
 				"English", "Art", "Drama", "Language", "Physical Education", "Band", "Anatomy", "Genetics"};
 		
-		private static String[] Major()={"Biological Science", "Math", "College Prep", "Gateway to Medicine", 
+		 String Major[] = {"Biological Science", "Math", "College Prep", "Gateway to Medicine", 
 				"Electrical Engineering", "Civil Engineering", "Chemical Engineering", "Industrial Design", 
 				"Physics","Architecture", "Aerospace Engineering"};
 		
-		private static String[] Guidance()={"Blain", "Taub", "Ambroise", "Barone", "Bosco", "Chan", "Blankson",
+		 String Guidance[] = {"Blain", "Taub", "Ambroise", "Barone", "Bosco", "Chan", "Blankson",
 				"Cuesta", "Holland", "Corsby", "Jordan", "Lam", "Lulov", "Paulson"};
 		
-		private static Teacher teacher;
-		private static Student[] students;
+		public Teacher, randomTeacher(Teacher, teacher)
+		{
+		     String subject[] = {"Anatomy","Genetics","Biology","Interpretive Dance","AP Studio Art","Linguistics","Underwater Basket Weaving","History of String","Manderin","AP Human Geo"};
+		     String title[] = {"Mr.","Mrs.","Dr.","Prof."};
+		     String lastName[] = {"Morri","Davis","Lina","Amato","Shuman","Rogers","Weiss"};
+		     String firstName[] = {"Lia","Nancy","Jessica","Amora","Jellisa","Damon","Stefan"};
+		    Teacher teacherx= new Teacher(3,firstName[new Random().nextInt(firstName.length)],
+		    		        lastName[new Random().nextInt(lastName.length)],91.0,subject[new Random().nextInt(subject.length)],title[new Random().nextInt(title.length)]);
+		     return teacherx ;
+		    }
+		public static void main(String[] args) 
+		{
+			Person teacher = new Teacher(91, "Lisa", "Damato", "Anatomy", "Ms", 100);
+			Person[] studentArr;
+			studentArr = new Person[32];
+			for (int i=0 ; i< studentArr.length;i++)	
+			{
+				studentArr[i]= randomStudent();	
+			}
 		
-		Teacher teacher1 = new Teacher (10, "Tom", "Moore", 91.3, "science", "Mr.");
-		
-	    Student student1 = new Student (3, "Lisa", "Smith", 97.2, 208644062, 9);
-	    
-	    Classroom classroom1 = new Classroom (students, teacher, subject );
-		
-	    public static void main (String [] args) 
-	    {
-			 firstNames=new String [4];
-			 
-			 familyNames=new String [4];
-			 
-			 firstNames [0] ="Eliza";
-			 
-			 familyNames [0]="Marie";
-			 
-			 firstNames [1]="Annie";
-			 
-			 familyNames [1]="Mac";
-			 
-			 firstNames [2]="Loura";
-			 
-			 familyNames [2]="Johnson";
-			 
-			 firstNames [3]="Brady";
-			 
-			 familyNames [3]="Williams";
-	    }
+        Classroom NewClass = new Classroom(studentArr, teacher);
+        
+		System.out.println(NewClass.printClass());
+		System.out.println(NewClass.classAverage());
 
-	}
-	public Student randomStudent()
-    {
-    	
-    }	
+		}
 }
